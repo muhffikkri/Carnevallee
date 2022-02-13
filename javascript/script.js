8125ef2f9da742d12114c8c3d6d38ceb380f93aa
Nexvaulus <moehammad.fikr@gmail.com>
2022-02-13 21:11:11 +0700

    Add animated navbar

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,20 @@
 $(window).scroll(function () {
-  const wScroll = $(this).scrollTop();
+    const wScroll = $(this).scrollTop();
 
-  // our class
-  if (wScroll > $("#our-class").offset().top - 400) {
-      $("#our-class .container .thumb").each(function(i) {
-        setTimeout(function () {
-            $("#our-class .container .thumb").eq(i).addClass("show");
-      }, 300 * (i + 1));
-    });
-  }
-  
-    
-  
-  console.log(wScroll);
-  if(wScroll < 5) {
-      console.log("hello");
-  }
+    // navbar
+    if (wScroll < 7) {
+        $(".navbar").removeClass("navbar-dark bg-primary shadow-sm p-3").addClass("navbar-light nav-transform");
+    } else {
+        $(".navbar").removeClass("navbar-light").addClass("navbar-dark bg-primary shadow-sm p-3");
+    }
+
+    // our class
+    if (wScroll > $("#our-class").offset().top - 600) {
+        $("#our-class .container .thumb").each(function (i) {
+            setTimeout(function () {
+                $("#our-class .container .thumb").eq(i).addClass("show");
+            }, 300 * (i + 1));
+        });
+    }
+
 });
