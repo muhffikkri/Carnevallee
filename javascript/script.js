0775cc30373b4d528e3475507c0f038cd6e937bc
Nexvaulus <moehammad.fikr@gmail.com>
2022-02-13 16:42:18 +0700

    yes

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,10 @@ $(window).scroll(function () {
   const wScroll = $(this).scrollTop();
 
   // our class
-  if (wScroll > $(".our-class").offset().top - 200) {
-      $(".our-class .container .thumb").each(function(i) {
+  if (wScroll > $("#our-class").offset().top - 250) {
+      $("#our-class .container .thumb").each(function(i) {
         setTimeout(function () {
-            $(".our-class .container .thumb").eq(i).addClass("show");
+            $("#our-class .container .thumb").eq(i).addClass("show");
       }, 300 * (i + 1));
     });
   }
