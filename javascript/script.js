11ad90e98e0d03bdea56ba2480aa662eb8bea619
Nexvaulus <moehammad.fikr@gmail.com>
2022-02-14 10:30:55 +0700

    Add nav

--- javascript/script.js
+++ javascript/script.js
@@ -8,6 +8,7 @@ $(window).scroll(function () {
         $(".navbar").removeClass("navbar-light").addClass("navbar-dark bg-primary shadow-sm p-3");
     }
 
+
     // our class
     if (wScroll > $("#our-class").offset().top - 600) {
         $("#our-class .container .thumb").each(function (i) {
@@ -18,3 +19,49 @@ $(window).scroll(function () {
     }
 
 });
+
+
+
+for (let i = 1; i <= 4; i++) {
+ console.log(       $(`#home .container .text:nth-child(${i})`));
+}
+
+
+
+
+
+function welcomeScreen() {
+
+    for (let i = 1; i <= 4; i++) {
+        setTimeout(function () {
+
+            $(`#home .container .text:nth-child(${i})`).css("transform", "translate(0, 0)");
+            $(`#home .container .text:nth-child(${i})`).css("opacity", "1");
+
+        }, 300 + (i + 200));
+
+
+    }
+
+
+
+
+    // $("#home .container .text").each(function (i) {
+    //     setTimeout(function () {
+    //         for (let i = 1; i <= 4; i++) {
+    //             $(`#home .container .text:nth-child(${i})`).eq(i).css("transform", "translate(0, 0)");
+    //             $(`#home .container .text:nth-child(${i})`).eq(i).css("opacity", "1");
+    //         }
+    //     }, 300 + (i + 200));
+    // });
+}
+
+setTimeout(function () {
+    welcomeScreen();
+
+
+    $("#home .container .image img").css("transform", "translate(0, 0)");
+    $("#home .container .image img").css("opacity", "1");
+
+
+}, 1000);
